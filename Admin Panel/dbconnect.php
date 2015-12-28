af325e9e268f93332b1e6b7caebb1c7456f80bd0
naresh10oct <naresh10oct@gmail.com>
2015-12-29 02:01:24 +0530

    Update dbconnect.php

--- Admin Panel/dbconnect.php
+++ Admin Panel/dbconnect.php
@@ -3,7 +3,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'poojadmi_store');
 define('DB_USER','root');
-define('DB_PASSWORD','will.1.iit');
+define('DB_PASSWORD','your_password');
 //echo "connecting to database";
 $con=mysql_connect(DB_HOST,DB_USER,DB_PASSWORD) or die("Failed to connect to MySQL: " . mysql_error());
 $db=mysql_select_db(DB_NAME,$con) or die("Failed to connect to MySQL: " . mysql_error());
